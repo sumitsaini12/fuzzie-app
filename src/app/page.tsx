d6ff6842539d21157c0e8f77deaf32fe722fbd32
sumit kumar <sumitsaini95488@gmail.com>
2024-11-07 13:34:09 +0530

    feat(ui): change tailwind css config and add shadcn components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import { Button } from "../components/ui/button";
 
 export default function Home() {
   return (
-    <div className="h-screen flex items-center justify-center gap-4">
+    <main className="h-screen flex items-center justify-center gap-4">
       <h1 className="flex items-center justify-center text-5xl font-semibold text-red-500">
         Fuzzie App Clone
       </h1>
       <Button>Submit</Button>
-    </div>
+    </main>
   );
 }
