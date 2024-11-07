c8000c28ced785b72544ca40aa833b2beedef25d
sumit kumar <sumitsaini95488@gmail.com>
2024-11-07 12:54:42 +0530

    fix(ui): shadcn component folder location issues fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "../components/ui/button";
+
 export default function Home() {
   return (
-    <h1 className="flex items-center justify-center text-5xl font-semibold text-red-500">
-      Fuzzie App Clone
-    </h1>
+    <div className="h-screen flex items-center justify-center gap-4">
+      <h1 className="flex items-center justify-center text-5xl font-semibold text-red-500">
+        Fuzzie App Clone
+      </h1>
+      <Button>Submit</Button>
+    </div>
   );
 }
